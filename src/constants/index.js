7c92e14eb3d4e7f8b0ae35222ab715ee9e30f715
Huei <aaa830714@gmail.com>
2019-08-26 15:02:34 +0800

    Add home page

---
+++ src/constants/index.js
@@ -0,0 +1,3 @@
+export const API = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-A54E517E-3603-4732-ABCE-23AAB4D7A554'
+
+export const SIX_CAPITAL = ['臺北市', '新北市', '桃園市', '臺中市', '臺南市', '高雄市']
